313bcddacc53365fa350af22f58062dd046b3e7a
Yosbel Marin <yosbel@yosbel.com>
2017-08-29 09:31:32 -0400

    add test for compilerOptions and use local tslint

--- spec/jasmine-ts.spec.ts
+++ spec/jasmine-ts.spec.ts
@@ -1,7 +1,14 @@
+import "jasmine"
+import * as js from "./inc.js"
+
 describe("jasmine-ts", () => {
 
   it("should work with TypeScript files", () => {
     expect(true).toBe(true);
   });
+
+  it("should parse --compilerOptions properly", () => {
+    expect(js).toBe(true);
+  })
 
 });
